987834d985f377069aa8580e8e636019bafe342d
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-04-14 13:06:24 +0300

    Step 6: multipule routes with parameters

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -3,4 +3,12 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Привет от ИСП-234! Автор: Медведев, Соколов");
 
+app.MapGet("/about", () => "Это мой первый ASP.NET Core сервер");
+
+app.MapGet("/time", () => $"Время на сервере: {DateTime.Now}");
+
+app.MapGet("/hello/{time}", (string name) => $"Привет, {name}!");
+
+app.MapGet("/sum/{a}/{b}", (int a, int b) => $"Cумма: {a + b}");
+
 app.Run();
